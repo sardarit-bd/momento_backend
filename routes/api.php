a51f7ea583c4b278b46f9631643886249c5e3197
funkyd3v <funkyd3v@gmail.com>
2026-04-28 16:56:55 +0600

    storage link done

--- routes/api.php
+++ routes/api.php
@@ -24,8 +24,6 @@ Route::post('forgotpass',      [OtpController::class, 'otpSender']);
 Route::post('verify',      [OtpController::class, 'verifyOtp']);
 Route::post('resetpass',      [OtpController::class, 'resetPassword']);
 
-// new route will be added here
-
 
 //Profile
 Route::get('profile/{id}',[ProfileController::class, 'profile']);

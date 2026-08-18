7bddd2f8eaa26520f1b946ec405b2b18deacd6da
SARDAR IT <sardarit.bd.official@gmail.com>
2026-08-18 12:58:11 +0600

    cors updated

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,16 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://192.168.25.165:3000', 'https://momentocardgames.com/', 'http://momentocardgames.com/'],
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://192.168.25.165:3000',
+        'https://momentocardgames.com',
+        'http://momentocardgames.com',
+        'https://backend.momentocardgames.com',
+    ],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true, // Set to true if sending cookies/auth headers
 ];
